47d68f5e8751b22044029f17099548ded428f2f8
Kaue-Dev <kauemassola900@gmail.com>
2025-03-19 21:30:03 -0300

    refactor: change import path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Input } from "@/components/input";
-import { Label } from "@/components/label";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
 	return (
