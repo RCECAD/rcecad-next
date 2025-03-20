297ccf81d3f645864bf7eece110f5462d9ae4bb3
Kaue-Dev <kauemassola900@gmail.com>
2025-03-19 21:30:52 -0300

    refactor: path change

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,20 @@
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+	children: React.ReactNode;
+	variant: ButtonVariants;
+  fullWidth?: boolean;
+}
+
+type ButtonVariants = "primary" | "secondary";
+
+export function Button({ children, variant, fullWidth, ...props }: ButtonProps) {
+	const buttonStyles = {
+		primary: "bg-sky-500 p-2.5 h-fit text-slate-100 font-bold hover:bg-sky-600",
+		secondary: "bg-slate-300 p-2.5 h-fit text-slate-800 font-medium hover:bg-slate-400",
+	};
+
+	return (
+		<button {...props} className={`${"rounded-lg cursor-pointer text-lg transition ease-in-out flex items-center justify-center text-center gap-2.5"} ${fullWidth && "w-full"} `.concat(buttonStyles[variant])}>
+			{children}
+		</button>
+	);
+}
