61ae8ea059d90dc058dd0d6187da49e211413bad
9gods <codingcomelli@gmail.com>
2025-03-21 00:08:28 -0300

    feat: implements toast component

---
+++ src/components/ui/toast.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+	const { theme = "system" } = useTheme();
+
+	return (
+		<Sonner
+			theme={theme as ToasterProps["theme"]}
+			className="toaster group"
+			toastOptions={{
+				classNames: {
+					toast:
+						"group toast group-[.toaster]:bg-slate-100 group-[.toaster]:text-slate-600 group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+					description: "group-[.toast]:text-slate-500",
+					actionButton:
+						"group-[.toast]:bg-sky-400 group-[.toast]:text-slate-100",
+					cancelButton:
+						"group-[.toast]:bg-red-400 group-[.toast]:text-slate-100",
+				},
+			}}
+			{...props}
+		/>
+	);
+};
+
+export { Toaster };
