e43287b179437658e70720bf4c7ade01a34c4ca0
9gods <codingcomelli@gmail.com>
2025-03-21 00:06:29 -0300

    fix: interface extends

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,24 +1,35 @@
 import type { JSX } from "react";
 
-interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	children: React.ReactNode;
 	variant: ButtonVariants;
-  fullWidth?: boolean;
+	fullWidth?: boolean;
 }
 
 type ButtonVariants = "primary" | "secondary";
 
 type ComponentSetup = (props: ButtonProps) => JSX.Element;
 
-export const Button: ComponentSetup = ({ children, variant, fullWidth, ...props }) => {
+export const Button: ComponentSetup = ({
+	children,
+	variant,
+	fullWidth,
+	...props
+}) => {
 	const buttonStyles = {
 		primary: "bg-sky-500 p-2.5 h-fit text-slate-100 font-bold hover:bg-sky-600",
-		secondary: "bg-slate-300 p-2.5 h-fit text-slate-800 font-medium hover:bg-slate-400",
+		secondary:
+			"bg-slate-300 p-2.5 h-fit text-slate-800 font-medium hover:bg-slate-400",
 	};
 
 	return (
-		<button {...props} className={`${"rounded-lg cursor-pointer text-lg transition ease-in-out flex items-center justify-center text-center gap-2.5"} ${fullWidth && "w-full"} `.concat(buttonStyles[variant])}>
+		<button
+			{...props}
+			className={`${"rounded-lg cursor-pointer text-lg transition ease-in-out flex items-center justify-center text-center gap-2.5"} ${fullWidth && "w-full"} `.concat(
+				buttonStyles[variant],
+			)}
+		>
 			{children}
 		</button>
 	);
-}
+};
