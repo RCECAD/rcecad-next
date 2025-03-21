bd1610d9dc96dc45ab3679618fa68246f8fb977a
Kaue-Dev <kauemassola900@gmail.com>
2025-03-21 20:23:27 -0300

    refactor?: tweak styles

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -3,12 +3,12 @@ import { TextHeader } from "@/components/ui/text-header";
 
 export default function Page() {
 	return (
-		<>
+		<div className="flex flex-col gap-8 w-full px-0 py-6 max-w-lg">
 			<TextHeader
 				title="Criar uma conta"
 				description="Insira os dados necessários para criar sua conta."
 			/>
 			<SignupForm />
-		</>
+		</div>
 	);
 }
