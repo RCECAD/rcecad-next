be9f78eba91d0f8464a91f2e921909d782b90b78
Kaue-Dev <kauemassola900@gmail.com>
2025-03-21 20:22:49 -0300

    refactor: tweak styles

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { AuthCard } from "@/components/ui/auth-card";
-import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
 
 const AuthLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
 	return (
-		<div className="w-screen h-screen md:px-24 md:py-32 2xl:px-48 2xl:py-32 items-center justify-center bg-slate-100">
+		<div className="bg-slate-100 h-screen flex items-center justify-center p-20">
 			<AuthCard>{children}</AuthCard>
 		</div>
 	);
