403bbe1b355ba6b17a5fbf65b7faa831ecac9282
Kaue-Dev <kauemassola900@gmail.com>
2025-03-21 20:23:39 -0300

    feat: implements signin form

---
+++ src/components/signin-form.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Eye, EyeOff } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { InputLabel } from "./ui/input-label";
+import { useState } from "react";
+import { Anchor } from "@/components/ui/anchor";
+
+export function SigninForm() {
+	const [showPassword, setShowPassword] = useState(false);
+  const inputType = showPassword ? "text" : "password";
+
+  function handleShowPassword() {
+    setShowPassword(!showPassword);
+  }
+
+	return (
+		<div className="flex flex-col gap-8">
+			<div className="flex flex-col gap-4">
+				<InputLabel htmlFor="email" label="Email">
+					<Input id="email" required type="email" />
+				</InputLabel>
+				<div className="flex flex-col gap-2">
+					<InputLabel htmlFor="password" label="Senha">
+						<Input id="password" required type={inputType} />
+						{showPassword ? (
+							<Eye className="absolute top-[58%] right-[15%] text-slate-400 cursor-pointer" onClick={handleShowPassword} />
+						) : (
+							<EyeOff className="absolute top-[58%] right-[15%] text-slate-400 cursor-pointer" onClick={handleShowPassword} />
+						)}
+					</InputLabel>
+					<Anchor href="/">Esqueceu a Senha?</Anchor>
+				</div>
+			</div>
+			<div className="flex flex-col gap-4 items-center justify-center">
+				<Button variant="primary" fullWidth>
+					Entrar
+				</Button>
+				<div className="flex items-center gap-2">
+					<p className="font-medium text-slate-400 text-lg">
+						Ainda não possui uma conta?
+					</p>
+					<Anchor href="/">Registre-se</Anchor>
+				</div>
+			</div>
+		</div>
+	);
+}
