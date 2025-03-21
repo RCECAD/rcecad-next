7c1df77b9d5e55fd94b4323649b4b85cb7b895b0
9gods <codingcomelli@gmail.com>
2025-03-20 22:12:09 -0300

    feat: implements auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,10 +1,14 @@
+"use client";
+import { AuthCard } from "@/components/ui/auth-card";
+import { usePathname } from "next/navigation";
+import type { ReactNode } from "react";
 
+const AuthLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+	return (
+		<div className="w-screen h-screen md:px-24 md:py-32 2xl:px-48 2xl:py-32 items-center justify-center bg-zinc-100">
+			<AuthCard>{children}</AuthCard>
+		</div>
+	);
+};
 
-export const AuthLayout = ({children}: Readonly<{children: ReactNode}>) => {
-  const pathname = usePathname();
-  return (
-    <div className="w-screen h-screen px-24 py-32 items-center justify-center bg-zinc-100">
-      {children}
-    </div>
-  )
-}
+export default AuthLayout;
