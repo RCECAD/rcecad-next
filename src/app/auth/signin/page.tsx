0a078fbc358dc15551339fa0e6e558797b0b0c31
Kaue-Dev <kauemassola900@gmail.com>
2025-03-21 20:23:05 -0300

    feat: implements signin page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,3 +1,16 @@
+import { SigninForm } from "@/components/signin-form";
+import { TextHeader } from "@/components/ui/text-header";
+import { LogIn } from "lucide-react";
+
 export default function Page() {
-	return <div className="text-slate-900">a</div>;
+	return (
+		<div className="flex flex-col gap-8 w-full px-0 py-6 max-w-lg">
+			<TextHeader
+				title="Entrar"
+				description="Insira seus dados para acessar sua conta"
+				icon={LogIn}
+			/>
+			<SigninForm />
+		</div>
+	);
 }
