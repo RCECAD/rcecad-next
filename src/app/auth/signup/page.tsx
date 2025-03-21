49125fff6cbdccc12b68c6d964e4c34a8198c131
9gods <codingcomelli@gmail.com>
2025-03-21 00:05:56 -0300

    feat: implements signup page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,3 +1,14 @@
+import { SignupForm } from "@/components/signup-form";
+import { TextHeader } from "@/components/ui/text-header";
+
 export default function Page() {
-	return <div>b</div>;
+	return (
+		<>
+			<TextHeader
+				title="Criar uma conta"
+				description="Insira os dados necessários para criar sua conta."
+			/>
+			<SignupForm />
+		</>
+	);
 }
