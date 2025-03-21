09cdf3320813d5b3e6ce3cac2dda3d3d71a27fa6
9gods <codingcomelli@gmail.com>
2025-03-21 00:08:35 -0300

    feat: implements user entity

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,11 @@
+type UserType = "person" | "business";
+
+export type User = {
+	id: string;
+	userType: UserType;
+	name: string;
+	cnpj: string;
+	cpf: string;
+	email: string;
+	password: string;
+};
