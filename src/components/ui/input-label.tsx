c57ddc6a50d2cfd9151f511262767ab0aeca0d28
9gods <codingcomelli@gmail.com>
2025-03-21 00:06:53 -0300

    refactor: rename input label

--- src/components/ui/input-label.tsx
+++ src/components/ui/input-label.tsx
@@ -1,13 +1,13 @@
 import type { JSX, LabelHTMLAttributes, ReactNode } from "react";
 
-interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
+interface InputLabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
 	label: string;
 	children: ReactNode;
 }
 
-type ComponentSetup = (props: LabelProps) => JSX.Element;
+type ComponentSetup = (props: InputLabelProps) => JSX.Element;
 
-export const Label: ComponentSetup = ({ children, label, ...rest }) => {
+export const InputLabel: ComponentSetup = ({ children, label, ...rest }) => {
 	return (
 		<div className="flex flex-col w-full h-fit gap-2">
 			<label {...rest} className="text-lg font-medium text-slate-700">
