bbeb3f12ae841cb2f7b4025c2d411e0f515f6739
Kaue-Dev <kauemassola900@gmail.com>
2025-03-27 16:29:49 -0300

    feat: implements signin schema

---
+++ src/schemas/signin-schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const signinSchema = z.discriminatedUnion("userType", [
+  z.object({
+	userType: z.literal("user"),
+	email: z.string().email("E-mail inválido"),
+		password: z
+			.string()
+			.min(8, "A senha deve possuir pelo menos 8 caracteres")
+			.regex(
+				/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).*$/,
+				"A senha deve conter uma letra maiúscula e um símbolo",
+			),
+  })
+])
+
+export type SigninFormValues = z.infer<typeof signinSchema>;
