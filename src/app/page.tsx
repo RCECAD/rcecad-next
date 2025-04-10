6e318be2016cb4ac666968a80fa9e224f520d51d
Kaue-Dev <kauemassola900@gmail.com>
2025-04-10 10:57:37 -0300

    refactor/feat: add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Input } from "@/components/ui/input";
-import { Anchor } from "@/components/ui/anchor";
+import { Sidebar } from "@/components/sidebar/sidebar";
 
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 p-24 row-start-2 items-center sm:items-start bg-zinc-200">
-				<Anchor href="/">Esqueceu a senha?</Anchor>
-			</main>
+		<div>
+			<Sidebar />
 		</div>
 	);
 }
