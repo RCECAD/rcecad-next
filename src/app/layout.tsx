605e6b21a8b3e2b4783d7291c7b46beeab01d849
Kaue-Dev <kauemassola900@gmail.com>
2025-04-10 10:57:18 -0300

    feat: add bg-slate-100

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${lato.className} ${lato.style} antialiased`}>
+			<body className={`${lato.className} ${lato.style} antialiased bg-slate-100`}>
 				<Toaster />
 				{children}
 			</body>
