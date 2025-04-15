8c84b6e2314fcba6acddb8c061b436a3ede05a56
Kaue-Dev <kauemassola900@gmail.com>
2025-04-15 19:56:12 -0300

    refactor: add file input component

--- src/app/(sidebar)/home/page.tsx
+++ src/app/(sidebar)/home/page.tsx
@@ -1,15 +1,9 @@
-import { FileWarning } from "lucide-react";
+import { FileInput } from "./components/file-input";
 
 export default function Home() {
 	return (
-    <div className="flex justify-center items-center h-screen w-full">
-      <div className="bg-slate-200 rounded-lg p-6 h-fit w-fit">
-        <FileWarning />
-        <p className="font-extrabold text-2xl">Nenhum projeto encontrado</p>
-        <p className="font-medium text-lg text-slate-500">
-          Crie um novo escolhendo uma das opções abaixo.
-        </p>
-      </div>
-    </div>
+		<div className="flex justify-center items-center h-screen w-full p-4">
+			<FileInput />
+		</div>
 	);
 }
